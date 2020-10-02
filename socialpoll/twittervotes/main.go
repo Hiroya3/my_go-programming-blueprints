64e58282304b9dced56c065b481cb98639fd7cd8
Terui Hiroya <terry@Mac-mini.local>
2020-10-02 21:40:26 +0900

    mongoDBからの読み込み

--- socialpoll/twittervotes/main.go
+++ socialpoll/twittervotes/main.go
@@ -1,3 +1,42 @@
 package main
 
+import (
+	"log"
+
+	"gopkg.in/mgo.v2"
+)
+
+var db *mgo.Session
+
+//mongoDBへの接続
+func dialdb() error {
+	var err error
+	log.Println("MongiDBにダイヤル中：localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+//mongoDBへの接続を解除
+func closedb() {
+	db.Clone()
+	log.Println("データベース接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	//ballotsデータベースに含まれるpollコレクション
+	//find(nil)はフィルタリングを行わない
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
 func main() {}
