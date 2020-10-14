ffc41bbb9ddf56297389e061e1b3a8058da09468
Hiroya3 <s.3.h.st.sp211@gmail.com>
2020-10-14 21:12:15 +0900

    コメントの追加

--- socialpoll/api/vars.go
+++ socialpoll/api/vars.go
@@ -6,6 +6,8 @@ import (
 )
 
 var (
+	//複数のリクエストが同時にアクセスするためlock機構が必要
 	varsLock sync.RWMutex
-	vars     map[*http.Request]map[string]interface{}
+	//key:*http.Request  value :別のmap(map[string]interface{})
+	vars map[*http.Request]map[string]interface{}
 )
