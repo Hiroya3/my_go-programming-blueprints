6c874b92f9c57572a97bfb5a0eef9734c194249c
Hiroya3 <s.3.h.st.sp211@gmail.com>
2020-10-14 21:16:18 +0900

    OpenVarsとCloseVarsを作成

--- socialpoll/api/vars.go
+++ socialpoll/api/vars.go
@@ -11,3 +11,19 @@ var (
 	//key:*http.Request  value :別のmap(map[string]interface{})
 	vars map[*http.Request]map[string]interface{}
 )
+
+func OpenVars(r *http.Request) {
+	varsLock.Lock()
+	if vars == nil {
+		vars = map[*http.Request]map[string]interface{}{}
+	}
+	vars[r] = map[string]interface{}{}
+	varsLock.Unlock()
+}
+
+//メモリリークを防ぐためメモリの解放を行う
+func CloseVars(r *http.Request) {
+	varsLock.Lock()
+	delete(vars, r)
+	varsLock.Unlock()
+}
