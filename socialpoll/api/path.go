04b7c363d7d127dd7fe57a812464846888021134
Hiroya3 <s.3.h.st.sp211@gmail.com>
2020-10-15 22:24:29 +0900

    コメント追加

--- socialpoll/api/path.go
+++ socialpoll/api/path.go
@@ -11,17 +11,18 @@ type Path struct {
 	ID   string
 }
 
+// /people/1/をイメージ
 func NewPath(p string) *Path {
 	var id string
 	//先頭末尾のスラッシュを削除
-	p = strings.Trim(p, PathSeparator)
+	p = strings.Trim(p, PathSeparator) // people/1
 	//残りの文字列をスラッシュで区切って分割
-	s := strings.Split(p, PathSeparator)
+	s := strings.Split(p, PathSeparator) // ["people",1]
 
 	if len(s) > 1 {
 		//スライスが複数ある場合は最後の項目をIDとする
-		id = s[len(s)-1]
-		p = strings.Join(s[:len(s)-1], PathSeparator)
+		id = s[len(s)-1]                              // 1
+		p = strings.Join(s[:len(s)-1], PathSeparator) // people/のみ。スライスは[{0}"people"{1}1{2}]なので全部含むには2が必要！
 	}
 	return &Path{Path: p, ID: id}
 }
