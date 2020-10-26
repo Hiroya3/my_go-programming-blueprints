512e3722ea475eacd5227fef882fd358e8dd153f
Hiroya3 <s.3.h.st.sp211@gmail.com>
2020-10-26 20:48:54 +0900

    main.goの作成

---
+++ meander/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"my_go-programming-blueprints/meander"
+	"net/http"
+	"runtime"
+)
+
+func main() {
+	//プログラムから利用できるCPU数の最大値を指定。今回は全てのCPUを利用するように指定(go1.4までは必要だった)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	//meander.APIKey = "TODO"
+	http.HandleFunc("/journeys", func(w http.ResponseWriter, r *http.Request) {
+		respond(w, r, meander.Journeys)
+	})
+	http.ListenAndServe(":8080", http.DefaultServeMux)
+}
+
+//抽象化
+func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
+	return json.NewEncoder(w).Encode(data)
+}
