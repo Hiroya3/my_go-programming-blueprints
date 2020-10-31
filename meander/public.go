58bd0f79949bb9206747f59a9aacef3254b26641
照井寛也 <s.3.h.st.sp211@gmail.com>
2020-10-31 20:29:53 +0900

    struct名の修正

--- meander/public.go
+++ meander/public.go
@@ -1,12 +1,12 @@
 package meander
 
-type Facede interface {
+type Facade interface {
 	Public() interface{}
 }
 
 func Public(o interface{}) interface{} {
 	//public()が実装されているかチェック
-	if p, ok := o.(Facede); ok {
+	if p, ok := o.(Facade); ok {
 		//実装されていたらPublicを呼び出し
 		//(Publicで返り値の見え方を定義することでユーザーにjsonの内容に変化を与えない)
 		return p.Public()
